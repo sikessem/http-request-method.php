58d80e6fe24e0bfc38c6b84c67fc59d41f79122f
SIGUI Kessé Emmanuel (https://sikessem.com) <contact@sikessem.com>
2021-09-13 06:51:44 +0200

    :tada: Start package components

---
+++ pkg/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6cd05747379ff66746f4dd779b4537f9c61c5d6e',
+        'name' => 'sikessem/http-method',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sikessem/http-method' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6cd05747379ff66746f4dd779b4537f9c61c5d6e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
